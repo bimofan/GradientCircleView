b66a3cd670a36131a90df93b4883e626e80fc815
Haikun Zhu <HaikunZhu@hello.local>
2015-04-15 16:06:59 +0800

    Initial Commit

---
+++ GradientCircleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GradientCircleView
+//
+//  Created by Haikun Zhu on 15/4/15.
+//  Copyright (c) 2015年 haikunzhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GradientCircleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GradientCircleView
+//
+//  Created by Haikun Zhu on 15/4/15.
+//  Copyright (c) 2015年 haikunzhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
