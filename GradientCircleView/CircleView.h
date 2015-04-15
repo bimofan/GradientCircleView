acba8ae4b25b21eaa5b424717937bc16c34f2357
Haikun Zhu <HaikunZhu@hello.local>
2015-04-15 16:53:21 +0800

    firstadd

---
+++ GradientCircleView/CircleView.h
@@ -0,0 +1,15 @@
+//
+//  CircleView.h
+//  GradientCircleView
+//
+//  Created by Haikun Zhu on 15/4/15.
+//  Copyright (c) 2015年 haikunzhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircleView : UIView
+
+-(id)initWithFrame:(CGRect)frame;
+
+@end
---
+++ GradientCircleView/CircleView.m
@@ -0,0 +1,46 @@
+//
+//  CircleView.m
+//  GradientCircleView
+//
+//  Created by Haikun Zhu on 15/4/15.
+//  Copyright (c) 2015年 haikunzhu. All rights reserved.
+//
+
+#import "CircleView.h"
+
+@implementation CircleView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+-(id)initWithFrame:(CGRect)frame
+{
+    if (self == [super initWithFrame:frame]) {
+        
+        
+        CAShapeLayer *shapelayer = [CAShapeLayer layer];
+        
+        
+        
+    }
+    
+    return self;
+    
+}
+
+
+
+
+//+(Class)layerClass
+//{
+//    
+//    return [CAGradientLayer class];
+//    
+//}
+
+@end
